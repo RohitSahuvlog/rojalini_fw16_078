c5bded33e0cf2fe4b17d866267cc21702012bd2d
RojaliniDas <100181657+RojaliniDas@users.noreply.github.com>
2022-04-25 00:36:30 +0530

    todo server
    initial commit

---
+++ unit-4/sprint-2/day-1/assignments/todo/src/components/Todoitem.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+// import './todo.css'
+
+const Todoitem =({data})=>{
+  return(
+    <>
+    <div className="a">
+    <h1>{data.title}</h1>
+    </div>
+    </>
+  )
+}
+export default Todoitem;
