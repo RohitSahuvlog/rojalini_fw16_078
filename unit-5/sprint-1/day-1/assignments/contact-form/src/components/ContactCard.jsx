8192f6150efa669c0dda483ea81fbc8929045eeb
RojaliniDas <100181657+RojaliniDas@users.noreply.github.com>
2022-05-18 20:22:21 +0530

    contact-form

--- unit-5/sprint-1/day-1/assignments/contact-form/src/components/ContactCard.jsx
+++ unit-5/sprint-1/day-1/assignments/contact-form/src/components/ContactCard.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './ContactCard.module.css'
 
 export const ContactCard = (props) => {
-    const {imgurl, name, email, id, phone} = props;
+
+  const {imgurl, name, email, phone} = props;
+  const [isActive, setisActive] = useState(false);
   return (
-    <div>
-      <div className={styles.container}>
+    
+    <div onClick={() => setisActive(!isActive)}
+       className={styles.container}>
           <img className={styles.pp} src={imgurl} alt="" />
           <div className={styles.data}>
-        <div>{name}</div>
-        <div>{email}</div>
-        </div>
+          <div>{name}</div>
+          <div>{email}</div>
+          {isActive && <div> {phone }</div>}
       </div>
     </div>
+    
   )
 }
 
